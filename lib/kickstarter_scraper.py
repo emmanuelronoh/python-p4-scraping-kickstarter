7afcf4fcfaa3aa635d9383b6603424a7f06c1177
emmanuel ronoh <eronoh036@gmail.com>
2024-10-07 08:59:01 +0300

    first commt

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,12 +1,6 @@
 from bs4 import BeautifulSoup
 import ipdb
 
-# projects: kickstarter.select("li.project.grid_4")[0]
-# title: project.select("h2.bbcard_name strong a")[0].text
-# image link: project.select("div.project-thumbnail a img")[0]['src']
-# description: project.select("p.bbcard_blurb")[0].text
-# location: project.select("ul.project-meta span.location-name")[0].text
-# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
 
 def create_project_dict():
     html = ''
@@ -23,7 +17,7 @@ def create_project_dict():
         'location': project.select("ul.project-meta span.location-name")[0].text,
         'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
         }
-    # return the projects dictionary
+    
 
     return projects
 
